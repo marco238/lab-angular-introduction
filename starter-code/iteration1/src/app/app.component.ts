f9ca8a02565bcd0befd698b71097c745b1d8a049
Marco <marco238cuba@gmail.com>
2018-03-03 12:47:23 +0100

    feat: Iteration1 done

--- starter-code/iteration1/src/app/app.component.ts
+++ starter-code/iteration1/src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = 'Welcome to my Album!';
+  album: Array<any> = [
+      {
+          imageURL: 'https://pbs.twimg.com/media/DTBgs5aXkAA4NXI.jpg',
+          description: 'Ironhack Madrid',
+          city: 'Madrid'
+      },
+      {
+          imageURL: 'https://pbs.twimg.com/media/CiHN8hVWUAAKxaK.jpg',
+          description: 'Ironhack Barcelona',
+          city: 'Barcelona'
+      },
+      {
+          imageURL: 'https://pbs.twimg.com/media/DOq1DqiXkAE5rfV.jpg',
+          description: 'Ironhack Cuba',
+          city: 'Cuba'
+      }
+  ]
 }
